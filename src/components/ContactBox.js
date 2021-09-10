5d5ee39641402e0d36701aacce7ffc57a6763c97
Bruno Pires <bruno.angeloti@terra.com.br>
2021-09-10 14:06:00 -0300

    Visual Pronto

--- src/components/ContactBox.js
+++ src/components/ContactBox.js
@@ -8,9 +8,13 @@ function ContactBox() {
 
     return (
         <div className="container">
+            <h1>CONTACT ME</h1>
             <form onSubmit={() => {sendEmail()}} className="form">
+                <label>Name</label>
                 <input type="text" className="name"/>
+                <label>Email</label>
                 <input type="email" className="email"/>
+                <label>Message</label>
                 <textarea type="text" className="message"/>
 
                 <button type="submit">Enviar</button>
