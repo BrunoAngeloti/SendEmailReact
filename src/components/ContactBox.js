e8fd5498ae6cbbf314c1de1884b9ead977252b95
Bruno Pires <bruno.angeloti@terra.com.br>
2021-09-10 16:11:40 -0300

    Enviando Email corretamente

--- src/components/ContactBox.js
+++ src/components/ContactBox.js
@@ -1,21 +1,31 @@
 import '../styles/components/ContactBox.css';
+import emailjs from "emailjs-com"
 
 function ContactBox() {
 
-    function sendEmail(){
-        console.log("cliquei");
+    function sendEmail(e){
+        e.preventDefault();
+        
+        emailjs.sendForm(
+            process.env.REACT_APP_SERVICE_EMAIL, 
+            process.env.REACT_APP_TEMPLATE_EMAIL, 
+            e.target,
+            process.env.REACT_APP_USER_EMAIL
+        ).then(res=>{
+            console.log(res)
+        }).catch(err=> console.log(err))
     }
 
     return (
         <div className="container">
             <h1>CONTACT ME</h1>
-            <form onSubmit={() => {sendEmail()}} className="form">
+            <form onSubmit={sendEmail} className="form">
                 <label>Name</label>
-                <input type="text" className="name"/>
+                <input required type="text" name="name" className="name"/>
                 <label>Email</label>
-                <input type="email" className="email"/>
+                <input required type="email" name="user_email" className="email"/>
                 <label>Message</label>
-                <textarea type="text" className="message"/>
+                <textarea required type="text" name="message" className="message"/>
 
                 <button type="submit">Enviar</button>
             </form>
